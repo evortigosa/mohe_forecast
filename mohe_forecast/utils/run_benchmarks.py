8fb996dc78d8810a9faae6b7b6ac5e23b837a957
evortigosa <evortigosa@gmail.com>
2026-05-16 12:17:09 +0300

    To run all benchmarks

--- mohe_forecast/utils/run_benchmarks.py
+++ mohe_forecast/utils/run_benchmarks.py
@@ -84,7 +84,7 @@ def build_parser():
     parser.add_argument("--verbose", action=argparse.BooleanOptionalAction, default=True,
                         help="Enable or disable text infos")
     # model
-    parser.add_argument("--model-size",  type=parse_value, default='base', help="Type of model configuration")
+    parser.add_argument("--model-size",  type=parse_value, default='tiny', help="Type of model configuration")
     parser.add_argument("--block-size",  type=int, default=672, help="Input sequence length / context window")
     parser.add_argument("--patch-width", type=int, default=8, help="Patch width")
     parser.add_argument("--width-factor",type=float, default=3, help="Output patch width")
@@ -132,13 +132,14 @@ def build_parser():
                         help="Enable or disable saving training/validation plots")
     parser.add_argument("--plot-cut-first", action=argparse.BooleanOptionalAction, default=True,
                         help="Enable or disable first epoch results in plot files")
+    parser.add_argument("--seed", type=parse_value, default=None, help="Random number generator seed")
 
     return parser
 
 
 def count_parameters(model) -> None:
     total_params= sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f'Number of model parameters: {total_params}')
+    print(f'Number of model parameters: {total_params:,}')
 
 
 def setup_model_from_checkpoint(filename, checkpoint_dir, device, verbose=True):
@@ -278,6 +279,8 @@ def setup_trainer(
 
 def main(device, use_fused, use_flashattn):
     args= build_parser().parse_args()
+    if args.seed is not None:
+        torch.manual_seed(int(args.seed))
     verbose= args.verbose
     if verbose:
         print(f"[INFO] Device: {device}; Using fused AdamW: {use_fused}; FlashAttention available: {use_flashattn}")
